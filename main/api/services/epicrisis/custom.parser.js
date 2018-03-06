2589dc4e17ded9a85180f3c040912a1357f7dac7
Shalimov <Shalimich@gmail.com>
2018-03-06 10:24:27 +0300

    Update custom parser

--- main/api/services/epicrisis/custom.parser.js
+++ main/api/services/epicrisis/custom.parser.js
@@ -1,6 +1,8 @@
 const expressions = require('angular-expressions')
 const moment = require('moment')
 
+const EMPTY_STRING = ''
+
 // define your filter functions here, for example, to be able to write {clientname | lower}
 expressions.filters.date = (date, format) => {
 	if (date) {
@@ -8,14 +10,17 @@ expressions.filters.date = (date, format) => {
 	}
 }
 
-expressions.filters.plusDays = (date, days, format) => {
+expressions.filters.addDays = (date, days, format) => {
 	if (date) {
 		return moment(date).add(days, 'days').format(format)
 	}
 }
 
-const customParser = (tag) => ({
-	get: tag === '.' ? i => i : (s) => expressions.compile(tag.replace(/’/g, '\''))(s),
+const customParser = tag => ({
+	get: tag === '.' ? i => i : s => {
+		const result = expressions.compile(tag.replace(/’/g, '\''))(s)
+		return result || EMPTY_STRING
+	},
 })
 
 module.exports = {

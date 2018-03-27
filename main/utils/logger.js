7f3a9cbdf60981364fd4fe50f6d88deecf431117
Shalimov <Shalimich@gmail.com>
2018-03-27 16:26:47 +0300

    Add simple file rotator + logger

--- main/utils/logger.js
+++ main/utils/logger.js
@@ -1,1 +1,17 @@
-module.exports = console
+const { transports, createLogger } = require('winston')
+
+require('winston-daily-rotate-file')
+
+const logger = createLogger({
+	transports: [
+		new transports.DailyRotateFile({
+			filename: 'logs/hearty-%DATE%.log',
+			datePattern: 'YYYY-MM-DD-HH',
+			zippedArchive: true,
+			maxSize: '20m',
+			maxFiles: '14d',
+		}),
+	],
+})
+
+module.exports = logger
